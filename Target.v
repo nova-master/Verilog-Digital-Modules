d82b23e7d29aff90dedbdfeb77df503c8742ae16
nova master <129145316+nova-master@users.noreply.github.com>
2023-03-29 15:53:18 +0530

    Update Target.v

--- Target.v
+++ Target.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/06/2023 01:00:28 PM
-// Design Name: 
-// Module Name: Target
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
