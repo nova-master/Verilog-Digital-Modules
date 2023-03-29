d348fc7dc4e2da8b63f46619dffb1462e6e69239
nova master <129145316+nova-master@users.noreply.github.com>
2023-03-29 15:54:56 +0530

    Update counterUD16L.v

--- counterUD16L.v
+++ counterUD16L.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/28/2023 02:55:58 PM
-// Design Name: 
-// Module Name: counterUD16L
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
