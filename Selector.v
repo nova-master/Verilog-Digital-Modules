46e9346023450f3b369e7c9b3c4abe389797c1bb
nova master <129145316+nova-master@users.noreply.github.com>
2023-03-29 15:49:58 +0530

    Update Selector.v

--- Selector.v
+++ Selector.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/26/2023 06:20:25 PM
-// Design Name: 
-// Module Name: Selector
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
