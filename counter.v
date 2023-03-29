510f28d43a214eeb8c7ed31b8c05b5ba1b0189d3
nova master <129145316+nova-master@users.noreply.github.com>
2023-03-29 15:54:35 +0530

    Update counter.v

--- counter.v
+++ counter.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/28/2023 10:29:27 AM
-// Design Name: 
-// Module Name: countUD4L
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
