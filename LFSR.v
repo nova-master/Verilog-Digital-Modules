9ab8282cc4ed03878774806b0056161d5b95898b
nova master <129145316+nova-master@users.noreply.github.com>
2023-03-29 15:48:53 +0530

    Update LFSR.v

--- LFSR.v
+++ LFSR.v
@@ -20,21 +20,3 @@ FDRE #(.INIT(1'b0)) Q7_FF (.C(clk), .R(1'b0), .CE(CE), .D(Q[6]), .Q(Q[7]));
 assign rnd=Q[3:0];
 endmodule
 
-//module LFSR(
-//input clk, 
-//output [7:0]rng
-//    );
-    
-
-//   wire [7:0] Q; 
-//FDRE #(.INIT(1'b0)) Q0_FF (.C(clk), .CE(1'b1), .D(Q[1]), .Q(Q[0]));
-//FDRE #(.INIT(1'b0)) Q1_FF (.C(clk), .CE(1'b1), .D(Q[2]), .Q(Q[1]));
-//FDRE #(.INIT(1'b0)) Q2_FF (.C(clk), .CE(1'b1), .D(Q[3]), .Q(Q[2]));
-//FDRE #(.INIT(1'b0)) Q3_FF (.C(clk), .CE(1'b1), .D(Q[4]), .Q(Q[3]));
-//FDRE #(.INIT(1'b0)) Q4_FF (.C(clk), .CE(1'b1), .D(Q[5]), .Q(Q[4]));
-//FDRE #(.INIT(1'b0)) Q5_FF (.C(clk), .CE(1'b1), .D(Q[6]), .Q(Q[5]));
-//FDRE #(.INIT(1'b0)) Q6_FF (.C(clk), .CE(1'b1), .D(Q[7]), .Q(Q[6]));
-//FDRE #(.INIT(1'b0)) Q7_FF (.C(clk), .CE(1'b1), .D(Q[6]~^Q[7]), .Q(Q[7]));
-
-//assign rng=Q;
-//endmodule
