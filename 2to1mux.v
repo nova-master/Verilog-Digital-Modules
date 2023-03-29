c84bd19863c6e224d114cf31606f292a0d04b30c
nova master <129145316+nova-master@users.noreply.github.com>
2023-03-29 15:44:01 +0530

    Update 2to1mux.v

--- 2to1mux.v
+++ 2to1mux.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/19/2023 05:05:58 PM
-// Design Name: 
-// Module Name: 2to1mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
